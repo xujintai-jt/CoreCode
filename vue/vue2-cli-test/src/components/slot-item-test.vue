8a31493d36a14c7a59612a9c6fec8b112e8a6bd6
xujintai-jt <1791639435@qq.com>
2021-01-22 22:19:54 +0800

    作用域插槽语法糖、解构赋值学习

--- vue/vue2-cli-test/src/components/slot-item-test.vue
+++ vue/vue2-cli-test/src/components/slot-item-test.vue
@@ -2,14 +2,14 @@
  * @Author: xujintai
  * @Date: 2021-01-22 20:04:24
  * @LastEditors: xujintai
- * @LastEditTime: 2021-01-22 20:22:57
+ * @LastEditTime: 2021-01-22 22:17:48
  * @Description: file content
  * @FilePath: \CoreCode\vue\vue2-cli-test\src\components\slot-item-test.vue
 -->
 <template>
   <div id="slot-item-test">
     <h2>slot-item-test start</h2>
-    <slot name="center" :item="item"></slot>
+    <slot name="center" :item="item" :msg="msg"></slot>
     <h1>-------------------</h1>
     <slot :item="item"></slot>
     <h2>slot-item-test end</h2>
@@ -21,6 +21,7 @@ export default {
   data() {
     return {
       item: "item-item",
+      msg: "hahaha",
     };
   },
 };
--- vue/vue2-cli-test/src/components/slot-test.vue
+++ vue/vue2-cli-test/src/components/slot-test.vue
@@ -2,7 +2,7 @@
  * @Author: xujintai
  * @Date: 2021-01-22 20:00:14
  * @LastEditors: xujintai
- * @LastEditTime: 2021-01-22 22:10:17
+ * @LastEditTime: 2021-01-22 22:19:15
  * @Description: file content
  * @FilePath: \CoreCode\vue\vue2-cli-test\src\components\slot-test.vue
 -->
@@ -11,7 +11,9 @@
     <h1>slot-test</h1>
     <slot-item-test name="数据数据">
       <!-- 语法糖 -->
-      <template #center="slotProps">{{'data1:'+data1}} {{'item:'+slotProps.item}}</template>
+      <!-- 1.作用域插槽语法糖：解构赋值 -->
+      <!-- 2.v-slot语法糖：# -->
+      <template #center="{item,msg}">{{'data1:'+data1}} {{'item:'+item}} {{'msg:'+msg}}</template>
       <template #default="slotProps">{{'default:'+data1}} {{'item:'+slotProps.item}}</template>
       <!-- <template v-slot:center="slotProps">{{'data1:'+data1}} {{'item:'+slotProps.item}}</template>
       <template v-slot:default="slotProps">{{'default:'+data1}} {{'item:'+slotProps.item}}</template>-->
