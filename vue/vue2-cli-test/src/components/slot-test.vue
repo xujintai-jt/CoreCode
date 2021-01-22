e1f7823ddc528abdb03f985903e99f5365287ee9
xujintai-jt <1791639435@qq.com>
2021-01-22 22:13:55 +0800

    作用域插槽的学习

--- vue/vue2-cli-test/src/components/slot-test.vue
+++ vue/vue2-cli-test/src/components/slot-test.vue
@@ -2,7 +2,7 @@
  * @Author: xujintai
  * @Date: 2021-01-22 20:00:14
  * @LastEditors: xujintai
- * @LastEditTime: 2021-01-22 20:29:35
+ * @LastEditTime: 2021-01-22 22:10:17
  * @Description: file content
  * @FilePath: \CoreCode\vue\vue2-cli-test\src\components\slot-test.vue
 -->
@@ -10,8 +10,11 @@
   <div id="slot-test">
     <h1>slot-test</h1>
     <slot-item-test name="数据数据">
-      <template v-slot:center="slotProps">{{'data1:'+data1}} {{'item:'+slotProps.item}}</template>
-      <template v-slot:default="slotProps">{{'default:'+data1}} {{'item:'+slotProps.item}}</template>
+      <!-- 语法糖 -->
+      <template #center="slotProps">{{'data1:'+data1}} {{'item:'+slotProps.item}}</template>
+      <template #default="slotProps">{{'default:'+data1}} {{'item:'+slotProps.item}}</template>
+      <!-- <template v-slot:center="slotProps">{{'data1:'+data1}} {{'item:'+slotProps.item}}</template>
+      <template v-slot:default="slotProps">{{'default:'+data1}} {{'item:'+slotProps.item}}</template>-->
     </slot-item-test>
   </div>
 </template>
