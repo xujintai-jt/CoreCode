36dda5a1d7c0f279787d3389edb14600e3001713
xujintai-jt <1791639435@qq.com>
2021-01-31 22:13:04 +0800

    promise复习

--- Promise/Promise_2021.1.31.js
+++ Promise/Promise_2021.1.31.js
@@ -2,61 +2,72 @@
  * @Author: xujintai
  * @Date: 2021-01-31 20:45:35
  * @LastEditors: xujintai
- * @LastEditTime: 2021-01-31 21:07:55
+ * @LastEditTime: 2021-01-31 22:12:19
  * @Description: file content
  * @FilePath: \CoreCode\Promise\Promise_2021.1.31.js
  */
 
-// new Promsie((resolve, reject) => {
+// new Promise((resolve, reject) => {
   // resolve()
 // })
-(() => {
+(function(window){
   const PENDING = 'pending'
   const RESOLVED = 'resolved'
   const REJECTED = 'rejected'
-  
-  function Promsie(excutor) {
+
+  function MyPromise(excutor) {
     const self= this
     this.status = PENDING
     this.storyFn = []
+    this.result = null
     
-    function resolve() {
-      if(this.status!==PENDING) return
+    function resolve(value) {
+      if (self.status !== PENDING)  return
+    
+      console.log('resolve函数');
       self.status = RESOLVED
+      self.result = value
       if (self.storyFn.length > 0) {
-        
+        setTimeout(() => {
+          
+        },0)
       }
     }
 
-    function reject() {
-      if(this.status!==PENDING) return
+    function reject(reason) {
+      if (self.status !== PENDING)  return
+      console.log('reject函数');
       self.status = REJECTED
+      self.result = reason
       if (self.storyFn.length > 0) {
-        
+        setTimeout(() => {
+          
+        },0)
       }
     }
 
     try {
-      excutor()
+      excutor(resolve,reject)
     }
-    catch(error) {
+    catch (error) {
+      console.log(error);
       reject(error)
     }
   }
 
-  Promsie.prototype.then = function (resolved,rejected) {
+  MyPromise.prototype.then = function (resolved,rejected) {
     const self = this
+    console.log(self);
     if (self.status === RESOLVED) {
       setTimeout(() => {
-        resolved()
+        resolved(self.result)
     },0)
     } else if(self.status === REJECTED) {
       setTimeout(() => {
-        rejected()
+        rejected(self.result)
     },0)
     }
-
-    
   }
   
-})()
+  window.MyPromise=MyPromise
+})(window)
